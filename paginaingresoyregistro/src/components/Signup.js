26b93cbd73daead4af43bd10f828096e591f06f4
Andresdavid <andresx1301@gmail.com>
2023-12-10 01:18:24 -0500

    -a

--- paginaingresoyregistro/src/components/Signup.js
+++ paginaingresoyregistro/src/components/Signup.js
@@ -17,7 +17,7 @@ const Signup = () => {
       <h2>Registrarse</h2>
       <form>
         <label>
-          NombreA:
+          Nombre:
           <input type="text" value={nombre} onChange={(e) => setnombre(e.target.value)} />
         </label>
         <br />
