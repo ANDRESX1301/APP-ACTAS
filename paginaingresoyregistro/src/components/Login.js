925a8d5609ed31205d93ad1ecfa1d602eb128ce2
Andresdavid <andresx1301@gmail.com>
2023-12-10 01:04:14 -0500

    first commit

---
+++ paginaingresoyregistro/src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Lógica para manejar la autenticación
+    console.log(`Iniciar sesión con email: ${email} y contraseña: ${password}`);
+  };
+
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      <form>
+        <label>
+          Correo Electrónico:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Iniciar Sesión
+        </button>
+      </form>
+      <p>
+        ¿No tienes una cuenta? <Link to="/signup">Registrarse</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ paginaingresoyregistro/src/components/Signup.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  const [apellido, setapellido] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = () => {
+    // Lógica para manejar el registro de usuario
+    console.log(`Registrarse con Nombre: ${apellido}, Apellido: ${lastName}, Email: ${email} y Contraseña: ${password}`);
+  };
+
+  return (
+    <div>
+      <h2>Registrarse</h2>
+      <form>
+        <label>
+          Nombre:
+          <input type="text" value={apellido} onChange={(e) => setapellido(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Apellido:
+          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Correo Electrónico:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Contraseña:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleSignup}>
+          Registrarse
+        </button>
+      </form>
+      <p>
+        ¿Ya tienes una cuenta? <Link to="/">Iniciar Sesión</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Signup;
